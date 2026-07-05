8efd28fe0649cb2d82db0602336aee5eac08e0a4
RhuanPSS <rhuanpeterson923@gmail.com>
2026-07-05 13:47:59 -0300

    sort sheipado a 2 km de sua casa

--- algoritmos/HeapSort.h
+++ algoritmos/HeapSort.h
@@ -1,9 +1,30 @@
 #ifndef HeapSort
 #define HeapSort
 
+// função auxiliar para o intro sort
 template<typename Iterador, typename Comparador>
 void heapSort(Iterador begin, Iterador end, Comparador cmp) {
-
+    // a ideia é fazer o heap pra cima pra cada subárvore (com exceção da raíz)
+    // depois pega a raíz e troca com o último elemento do vetor
+    // repete o processo mas sem este último elemento e por aí vai :D
+     while (begin < end) {
+        int sz = end - begin;
+        for (int idx = 1; idx < sz; ++idx) {
+            int last = idx;
+            int pai = (idx - 1) / 2;
+            while (pai != last && cmp(*(begin + pai), *(begin + last))) {
+                auto aux = *(begin + last);
+                *(begin + last) = *(begin + pai);
+                *(begin + pai) = aux;
+                last = pai;
+                pai = (pai - 1) / 2;
+            }
+        }
+        auto aux = *begin;
+        *begin = *(end - 1);
+        *(end - 1) = aux;
+        --end;
+     }
 }
 
 template<typename Iterador>
--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
-#include "estruturas/MaxHeap.h"
-#include "algoritmos/InsertionSort.h"
+#include "estruturas/Vetor.h"
+#include "algoritmos/HeapSort.h"
 using namespace std;
 
 int main() {
     // bo depurar as parada na main
 
-    maxHeap<int> pq;
-    pq.push(2);
-    pq.push(6);
-    pq.push(8);
-    pq.push(3);
-    pq.push(1);
-    cout  << pq.front() << '\n';
-
-    while (!pq.empty()) {
-        int f = pq.pop();
-        cout << f << ' ';
+    int n = 10;
+    vetor<int> vet;
+    for (int i = 1; i <= n; ++i) {
+        int j = (i + 3) % n + 1;
+        vet.push_back(j);
     }
+    for (int i : vet) cout << i << ' ';
+    cout << '\n';
+    heapSort(vet.begin(), vet.end());
+    for (int i : vet) cout << i << ' ';
     cout << '\n';
 
     return 0;
