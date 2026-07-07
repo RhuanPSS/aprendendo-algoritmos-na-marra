229948d209eeb0307afebfdd781484b64e2d5357
RhuanPSS <rhuanpeterson923@gmail.com>
2026-07-07 00:45:05 -0300

    fix: typos

--- matematicas/Modular.h
+++ matematicas/Modular.h
@@ -23,8 +23,8 @@ int64_t inv_mod(int64_t a, int64_t b, const int64_t mod) {
     // Se o módulo não for primo, daí pedimos ajuda pro algoritmo extendido de Euclides
     int64_t aux, k;
     int64_t gcd = ex_euclides(b, mod, aux, k);
-    if (gcd != 1) return -1; // se 'a' e 'b' não forem coprimos, não existe modular inverso
-    int64_t inv = (aux % mod + mod) % mod; // as vezes x pode ser negativo, daí é só somar mod dnv
+    if (gcd != 1) return -1; // se 'b' e 'mod' não forem coprimos, não existe modular inverso
+    int64_t inv = (aux % mod + mod) % mod; // as vezes o inverso pode ser negativo, daí é só somar mod dnv
     a %= mod;
     return (a * inv) % mod;
 }
