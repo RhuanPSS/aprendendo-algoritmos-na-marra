63eb554342ba917ea7def0050bb879f6bc807cf9
RhuanPSS <rhuanpeterson923@gmail.com>
2026-09-17 19:15:54 -0300

    feat: funções max/min

--- estruturas/Comparadores.h
+++ estruturas/Comparadores.h
@@ -9,10 +9,16 @@ struct maior {
 };
 
 template<typename T>
+bool max(T a, T b) { return a > b; }
+
+template<typename T>
 struct menor {
     bool operator()(const T& a, const T& b) const {
         return a < b;
     }
 };
+
+template<typename T>
+bool min(T a, T b) { return a < b; }
 
 #endif
